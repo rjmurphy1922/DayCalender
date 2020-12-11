85981cdc6e304908af85dbc3a06510966d2de757
Ryan <ryan@Ryans-iMac.local>
2020-12-10 19:25:04 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+
+// var now = moment().hour()
+// var myTime = setInterval(myTime,1000);
+// var curHour = moment().hour()
+
+// //     var nine = $("#9").attr("data-value")
+// //     var ten = $("#10").attr("data-value")
+// //     var elleven = $("#11").attr("data-value")
+// //     var twelve = $("#12").attr("data-value")
+// //     var one = $("#1").attr("data-value")
+// //     var two= $("#2").attr("data-value")
+// //     var three = $("#3").attr("data-value")
+// //     var four = $("#4").attr("data-value")
+// //     var five = $("#5").attr("data-value")
+
+// //   calArray = [nine,ten,elleven,twelve,one,two,three,four,five]
+
+// // //   for (i = 0; i < calArray.length; i++){
+
+// // //   console.log(calArray[i])
+
+// // //   }
+
+// function myTime(){
+//   var timerID = moment().format('MMMM Do YYYY, h:mm:ss a')
+//   document.getElementById("currentDay").innerHTML=timerID;
+// }
+
+
+
+
+// timeArray=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+// console.log(curHour)
+
+
+    
+//     var nine = $("#9").attr("data-value")
+//     var ten = $("#10").attr("data-value")
+//     var elleven = $("#11").attr("data-value")
+//     var twelve = $("#12").attr("data-value")
+//     var one = $("#1").attr("data-value")
+//     var two= $("#2").attr("data-value")
+//     var three = $("#3").attr("data-value")
+//     var four = $("#4").attr("data-value")
+//     var five = $("#5").attr("data-value")
+
+//   calArray = [nine,ten,elleven,twelve,one,two,three,four,five]
+//   console.log(ten)
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+/* <script src= "https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script> */
